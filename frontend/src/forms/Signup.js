79656dbc502fa4b43877f68a9bcc4252075b31b6
isaacjosh71@gmail.com <isaacjosh71@gmail.com>
2024-12-30 17:51:47 +0100

    latest update

--- frontend/src/forms/Signup.js
+++ frontend/src/forms/Signup.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useSignup } from "../hooks/useSignup";
 import logo from "../img/logo512.png";
+import { Circles, Rings, ThreeDots } from "react-loader-spinner";
 
 const { useState } = require("react");
 
@@ -10,9 +11,15 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [revealed, setRevealed] = useState(true);
   const { signup, error, isLoading } = useSignup();
+  const [loading, setLoading] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
     await signup(username, email, password);
   };
@@ -23,8 +30,9 @@ const Signup = () => {
 
   return (
     <div className="flex justify-around items-center bg-gradient-to-tr from-gray-900 to-black h-screen font-nunito">
-      
-      <h2 className="font-afacadFlux text-yellow-400 font-extrabold text-2xl underline -rotate-0 relative z-0 bottom-64 md:left-96 animate-pulse">becomeIngenious</h2>
+      <h2 className="font-afacadFlux text-yellow-400 font-extrabold text-2xl underline -rotate-0 relative z-0 bottom-64 md:left-96 animate-pulse">
+        becomeIngenious
+      </h2>
 
       <form
         className="bg-transparent p-6 rounded-md shadow-lg md:w-screen scale-75 absolute z-50"
@@ -80,22 +88,18 @@ const Signup = () => {
               >
                 {revealed ? (
                   <>
-                    <path
-                      fillRule="evenodd"
-                      d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
-                      clipRule="evenodd"
-                    />
+                    <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+                    <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+                    <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
                   </>
                 ) : (
                   <>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="size-6"
-                    >
-                      <path d="M18 1.5c2.9 0 5.25 2.35 5.25 5.25v3.75a.75.75 0 0 1-1.5 0V6.75a3.75 3.75 0 1 0-7.5 0v3a3 3 0 0 1 3 3v6.75a3 3 0 0 1-3 3H3.75a3 3 0 0 1-3-3v-6.75a3 3 0 0 1 3-3h9v-3c0-2.9 2.35-5.25 5.25-5.25Z" />
-                    </svg>
+                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                      clipRule="evenodd"
+                    />
                   </>
                 )}
               </svg>
@@ -108,8 +112,16 @@ const Signup = () => {
             disabled={isLoading}
             className="text-xl text-center font-semibold cursor-pointer text-white border-0 rounded-md p-2 bg-blue-600 transition ease-out duration-500 hover:scale-125"
           >
-            Sign up
+            {loading ? "Authenticating..." : "Sign up"}
           </button>
+          {loading && (
+            <ThreeDots
+              height={50}
+              width={50}
+              color="lightBlue"
+              ariaLabel="loading"
+            />
+          )}
         </div>
         {error && (
           <div className="text-red-500 mx-auto bg-gray-200 border-2 p-2 rounded-2xl w-fit">
